a3074f337acae9bbacfab54e8a8e4447edf4e736
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2023-03-12 13:02:07 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Sistem_Programe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine($"Час: {currentTime.ToLongTimeString()}");
+            Console.WriteLine($"Дата: {currentTime.ToLongDateString()}\n");
+
+            
+            Task task1 = new Task(() => Console.WriteLine($"{DateTime.Now.ToLongTimeString()}"));
+            task1.Start();
+
+            
+            Task task2 = Task.Factory.StartNew(() => Console.WriteLine($"{DateTime.Now.ToLongTimeString()}"));
+
+           
+            Task task3 = Task.Run(() => Console.WriteLine($"{DateTime.Now.ToLongTimeString()}"));
+
+            
+            Task.WaitAll(task1, task2, task3);
+        }
+    }
+}
